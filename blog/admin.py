32cde04cff2cc73dc1eca6822b447549056598f0
Yusuf Teker <y.teker.17@protonmail.com>
2021-08-24 15:46:57 +0300

    kategori modeli olusturuldu

---
+++ blog/admin.py
@@ -0,0 +1,5 @@
+from blog.models.kategori import KategoriModel
+from django.contrib import admin
+from blog.models import KategoriModel
+# Register your models here.
+admin.site.register(KategoriModel)
